3d9c4fb853aa69520ab4ccec66dc02f63c793641
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-08-03 19:41:23 -0400

    Sending our new item when the user submits the form (presses enter) but avoids refresh

--- src/App.tsx
+++ src/App.tsx
@@ -13,14 +13,16 @@ type TodoItemType = {
 export function App() {
   // Step 2, after static implementation, set the state.
   const [todoItems, setTodoItems] = useState<TodoItemType[]>([])
+  // 2nd state to set. Manages input text from user.
+  const [newTodoText, setNewTodoText] = useState('')
 
-  // useEffect has a non-async function
+  // useEffect has a non-async function, Loads our data ONCE.
   useEffect(function () {
     // Our async function inside!
     async function fetchListOfItems() {
       console.log('this runs when the component first mounts')
       const response = await axios.get(
-        'https://one-list-api.herokuapp.com/items?access_token=cohort26'
+        'https://one-list-api.herokuapp.com/items?access_token=cohort25'
       )
 
       if (response.status === 200) {
@@ -29,7 +31,22 @@ export function App() {
     }
     //
     fetchListOfItems()
+    // Don't forget empty array!
   }, [])
+
+  async function handleCreateNewTodoItem() {
+    const body = {
+      item: { text: newTodoText },
+    }
+    console.log(`Time to create a todo: ${newTodoText}`)
+    const response = await axios.post(
+      'https://one-list-api.herokuapp.com/items?access_token=cohort25',
+      body
+    )
+    if (response.status === 201) {
+      console.log(response.data)
+    }
+  }
 
   return (
     <div className="app">
@@ -49,8 +66,23 @@ export function App() {
             )
           })}
         </ul>
-        <form>
-          <input type="text" placeholder="Whats up?" />
+        {/* <Form> enables on submit "enter". Subtle but important for input. All forms by default want to be submitted. We need somewhere to submit to... */}
+        <form
+          onSubmit={function (event) {
+            // This says, please form don't do your usual behavior to avoid re-render.
+            // I will tell you what to do
+            event.preventDefault()
+            handleCreateNewTodoItem()
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Whats up?"
+            value={newTodoText}
+            onChange={function (event) {
+              setNewTodoText(event.target.value)
+            }}
+          />
         </form>
       </main>
       <footer>
