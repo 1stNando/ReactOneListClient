1dd1793ecd5248a02a9be20b80c83b09bec910e0
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-08-22 17:05:07 -0400

    Installed npm, and imported QueryClient to use instead of Axios for URL routing.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.scss'
 import { App } from './App'
+import { QueryClient } from 'react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.render(
   <React.StrictMode>
