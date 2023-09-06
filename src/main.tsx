7ccf12ad765beb16da4cfc01078eb40eef2a77b4
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-09-05 20:45:02 -0400

    Set up reactQuery at the main.tsx.

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom'
 import './index.scss'
 import { App } from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
